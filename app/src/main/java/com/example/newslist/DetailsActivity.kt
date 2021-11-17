a67a4028338be4f7e98d4285b59f33f66068824a
Martin Pottiez <martinpottiez@hotmail.fr>
2021-11-17 11:43:31 +0100

    [Details] Add Ok button

--- app/src/main/java/com/example/newslist/DetailsActivity.kt
+++ app/src/main/java/com/example/newslist/DetailsActivity.kt
@@ -1,12 +1,20 @@
 package com.example.newslist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         setTitle(getLocalClassName())
+
+        val okButton = findViewById<Button>(R.id.ok)
+        okButton.setOnClickListener {
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
